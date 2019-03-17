a3586f5b03bd9fa741e7e576d9e8a6b49b05ee97
Valeriy Bezuglyy <valnocorner@gmail.com>
2019-03-17 17:43:48 +0300

    mapper

--- Demo-PlotView/data/ChartMapper.swift
+++ Demo-PlotView/data/ChartMapper.swift
@@ -9,20 +9,47 @@
 import Foundation
 
 class ChartMapper {
-    func map(from json: [[AnyHashable: Any]]) throws -> [Chart] {
+    func map(from json: [[AnyHashable: Any]]) -> [Chart] {
         var result: [Chart] = []
         
         for object in json {
             //  columns
             guard let objectColumns = object["columns"] as? [[Any]] else { continue }
-            var columns: [String: [Int]] = [:]
+            do {
+                //  check for equal count of x and y values
+                let set = Set(objectColumns.map({$0.count}))
+                guard set.count == 1 else { continue }
+            }
+            
+            // [["x", 1, 2, 3], ["y", 4, 5, 6]] => ["x": [1,2,3], "y": [4,5,6]]
+            var columns: [String: [Double]] = [:]
             for item in objectColumns {
                 guard let key = item.first as? String,
-                    let value = Array(item.dropFirst()) as? [Int] else { continue }
+                    let value = Array(item.dropFirst()) as? [Double] else { continue }
                 columns[key] = value
             }
             
-            guard let objectTypes = object["types"] as? [String: String] else { continue }
+            //  find name of "x" column
+            guard let columnToType = object["types"] as? [String: String],
+                let xKey = columnToType.first(where: { $0.value == "x" }).map({ $0.key }),
+                let xValues = columns[xKey] else { continue }
+            columns.removeValue(forKey: xKey)
+            let yValues = columns
+            
+            //  other
+            guard let names = object["names"] as? [String: String] else { continue }
+            guard let colorsHex = object["colors"] as? [String: String] else { continue }
+            
+            //  create charts
+            let lines: [ChartLine] = yValues.compactMap({
+                guard let name = names[$0.key],
+                    let hex = colorsHex[$0.key] else { return nil }
+                return ChartLine(values: $0.value,
+                                 colorHex: hex,
+                                 name: name)
+            })
+            let chart = Chart(x: xValues, lines: lines)
+            result.append(chart)
         }
         
         return result
