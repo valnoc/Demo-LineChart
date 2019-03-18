5453bf1ac1c2ce1a23451ebef57b1debe259b3ae
Valeriy Bezuglyy <valnocorner@gmail.com>
2019-03-18 12:54:43 +0300

    try to draw window

--- Demo-PlotView/ui/x range control/LineChartXRangeControl.swift
+++ Demo-PlotView/ui/x range control/LineChartXRangeControl.swift
@@ -12,12 +12,18 @@ class LineChartXRangeControl : UIControl {
     
     let chartView: LineChartView
     
+    let windowVerticalBorderWidth: CGFloat = 3
+    let windowHorizontalBorderWidth: CGFloat = 11
+    let windowLeftX: CGFloat = 10
+    let windowRightX: CGFloat = 50
+    
     init(chart: LineChart) {
         self.chartView = LineChartView(chart: chart)
         super.init(frame: .zero)
         
         chartView.translatesAutoresizingMaskIntoConstraints = false
         addSubview(chartView)
+        backgroundColor = .yellow
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -27,7 +33,31 @@ class LineChartXRangeControl : UIControl {
     //MARK: -
     override func layoutSubviews() {
         super.layoutSubviews()
-        chartView.frame = bounds
+        chartView.frame = bounds.insetBy(dx: 0, dy: windowVerticalBorderWidth)
+    }
+ 
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        guard let ctx = UIGraphicsGetCurrentContext() else { return }
+        
+        drawWindow(rect, ctx)
     }
     
+    func drawWindow(_ rect: CGRect, _ ctx: CGContext) {
+        ctx.saveGState()
+        
+        ctx.setFillColor(UIColor(red: 195.0 / 255.0,
+                                 green: 206.0 / 255.0,
+                                 blue: 217.0 / 255.0,
+                                 alpha: 1).cgColor)
+        ctx.addPath(CGPath(roundedRect: CGRect(x: windowLeftX,
+                                               y: 0,
+                                               width: windowRightX - windowLeftX,
+                                               height: rect.height),
+                           cornerWidth: 1,
+                           cornerHeight: 1,
+                           transform: nil))
+        ctx.fillPath()
+        ctx.restoreGState()
+    }
 }
