b1feebadb334f00840f5478768057125219968f4
Valeriy Bezuglyy <valnocorner@gmail.com>
2019-03-21 22:39:48 +0300

    added remderers

---
+++ Demo_LineChart/ui/statistics/chart view/renderers/LineChartLinesRenderer.swift
@@ -0,0 +1,13 @@
+//
+//  LineChartLinesRenderer.swift
+//  Demo_LineChart
+//
+//  Created by Valeriy Bezuglyy on 21/03/2019.
+//  Copyright © 2019 Valeriy Bezuglyy. All rights reserved.
+//
+
+import Foundation
+
+class LineChartLinesRenderer {
+    
+}
---
+++ Demo_LineChart/ui/statistics/chart view/renderers/LineChartYAxisRenderer.swift
@@ -0,0 +1,13 @@
+//
+//  LineChartYAxisRenderer.swift
+//  Demo_LineChart
+//
+//  Created by Valeriy Bezuglyy on 21/03/2019.
+//  Copyright © 2019 Valeriy Bezuglyy. All rights reserved.
+//
+
+import Foundation
+
+class LineChartYAxisRenderer {
+    
+}
